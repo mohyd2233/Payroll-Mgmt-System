d86f75732205949929536f672854a9de7f408f7f
mohyd2233 <mhydersiddiqui0808@gmail.com>
2024-11-28 20:02:39 +0530

    commit

--- app.py
+++ app.py
@@ -1,78 +1,76 @@
-# importing libraries
-from employee_details import employee
-from employee_details import salarycalculator
-from flask import Flask,render_template,jsonify,request
-
-# creating ojects
+from flask import Flask, render_template,jsonify,request
+from employee import Employee,SalaryCalculator
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/employee_signup',methods = ['GET','POST'])
-def emp_signup():
+@app.route('/employee/signup', methods=['GET','POST'])
+def signup():
     if request.method=='POST':
         eid = request.form.get('eid')
         ename = request.form.get('ename')
         dptid = request.form.get('dptid')
         designation = request.form.get('designation')
-        emailid = request.form.get('emailid')
-        contact_no = request.form.get('contact_no')
+        email = request.form.get('email')
+        contact = request.form.get('contact')
         address = request.form.get('address')
         print(eid,ename,dptid)
-        emp = employee()
-        emp.empinsert(eid=eid,ename=ename,dptid=dptid,designation=designation,emailid=emailid,contact_no=contact_no,address=address)
+        emp = Employee()
+        emp.empinsert(eid = eid,ename=ename,dptid = dptid,designation=designation,email=email,contact=contact,address=address)
         return render_template('message.html')
     else:
         return render_template('signup.html')
+
 @app.route('/employees',methods = ['GET','POST'])
 def show_employees():
-    emp = employee()
+    emp = Employee()
     data = emp.show_employees()
     return render_template('showemployees.html',employees = data)
 
 @app.route('/attendance',methods = ['GET','POST'])
 def attendance():
     if request.method=='POST':
-        dptid= request.form.get('dptid')
-        dpt_name = request.form.get('dpt_name')
-        eid = request.form.get('eid')
-        ename = request.form.get('ename')
-        date= request.form.get('date')
-        time_in = request.form.get('time_in')
-        time_out = request.form.get('time_out')
-        print(eid,ename,dptid)
-        emp = employee()
-        emp.attendance(dptid=dptid,dpt_name=dpt_name,eid=eid,ename=ename,date=date,time_in=time_in,time_out=time_out)
-        return render_template('index.html')
-    return render_template('attendance.html')
-@app.route('/salary_details',methods = ['GET','POST'])
-def salary_details():
-    if request.method=='POST':
-        eid=request.form.get('eid')
-        dptid= request.form.get('dptid')
-        account_number=request.form.get('account_number')
-        PAN = request.form.get('PAN')
-        Base_salary = request.form.get('Base_salary')
-        emp = employee()
-        emp.salary_details(eid=eid,dptid=dptid,account_number=account_number,PAN=PAN,Base_salary=Base_salary)
+        dptid = request.form.get("dptid")
+        dptname = request.form.get("dptname")
+        eid= request.form.get("eid")
+        ename= request.form.get("ename")
+        date= request.form.get("date")
+        timein= request.form.get("timein")
+        timeout= request.form.get("timeout")
+
+        emp = Employee()
+        emp.attendance(dptid = dptid,dptname = dptname,eid = eid,ename = ename,date = date,timein = timein,timeout = timeout)
         return render_template('message.html')
-    return render_template('enter_salary.html')
-@app.route('/payroll_release',methods = ['GET','POST'])
-def payroll_release():
-     if request.method=='POST':
+    return render_template('attendance.html')
+
+@app.route('/salary/details',methods = ['GET','POST'])
+def salarydetails():
+    if request.method=='POST':
+        eid = request.form.get("eid")
+        dptid = request.form.get("dptid")
+        acc_no = request.form.get("account_number")
+        pan = request.form.get("pan")
+        bs = request.form.get("base_salary")
+        emp = Employee()
+        emp.salaryinsert(eid = eid,dptid = dptid,account_number = acc_no,pan = pan, base_sallary = bs)
+        return render_template('message.html')
+    else:
+        return render_template('salarydetails.html')
+    
+@app.route('/payroll/release',methods = ['GET','POST'])
+def payrollrelease():
+    if request.method=='POST':
         eid = request.form.get("empid")
-        sc = salarycalculator()
-        total_salary = sc.salarycalculation(eid = int(eid))
-        context = {'EmployeeID':eid,'TotalSalary':int(total_salary)}
+        sc = SalaryCalculator()
+        total_sallary = sc.salarycalculation(eid = int(eid))
+        context = {'EmployeeID':eid,'TotalSalary':int(total_sallary)}
         return render_template('showsalary.html',data = context)
-     else:
-       return render_template('enter_eid.html')
+    else:
+        return render_template('empid.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5050)
+if __name__=='__main__':
+    app.run(host = '0.0.0.0',port = 5050)
 
-# creating object for class
 
-#calling insert function
-#emp.empinsert(eid=1,ename="Bhuvan",dptid=11,designation="Manager",emailid="bhuvan@gmail.com",contact_no=9876543210,address="Mumbai")
